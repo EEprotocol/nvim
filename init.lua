4180fae1c4b51e99342fb205c13f92016f1e8774
EEprotocol <kei310c@outlook.com>
2024-02-13 22:48:02 +0900

    [Modify] session setting

--- init.lua
+++ init.lua
@@ -16,12 +16,14 @@ vim.o.hlsearch=true								--high light for search
 --local setting
 vim.opt.undofile=true
 vim.opt.autochdir=true            --change the work directory automatically
+vim.opt.wrap=false               --set no wrap
 local option={
 	encoding="utf-8",
 	fileencoding="utf-8",
 	undofile=true,
 	undodir="~/.config/nvim"
 }
+vim.loader.enable()
 
 -- キーマッピング
 vim.api.nvim_set_keymap('n', '<Space>', ':nohlsearch<CR>', { noremap = true, silent = true })
@@ -593,7 +595,7 @@ require('session_manager').setup({
   sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
   session_filename_to_dir = session_filename_to_dir, -- Function that replaces symbols into separators and colons to transform filename into a session directory.
   dir_to_session_filename = dir_to_session_filename, -- Function that replaces separators and colons into special symbols to transform session directory into a filename. Should use `vim.loop.cwd()` if the passed `dir` is `nil`.
-  autoload_mode = config.AutoloadMode.LastSession, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+  autoload_mode = config.AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
   autosave_last_session = true, -- Automatically save last session on exit and on session switch.
   autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
   autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.
