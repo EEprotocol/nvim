3de2f1978865a1ea31ffe6adaae1b2ccc62b82e8
EEprotocol <kei310c@outlook.com>
2024-04-02 22:32:48 +0900

    [Modify] markdown previewer

--- init.lua
+++ init.lua
@@ -199,8 +199,6 @@ require("lazy").setup(
 		event="VimEnter"},
 		{"EEprotocol/Arduineovim",
 		event="VimEnter"};
-    {"toppair/peek.nvim",
-		event="Vimenter"},
 		{"EEprotocol/Arduineovim"},
 		{"toppair/peek.nvim",
 		event={"VeryLazy"},
@@ -216,12 +214,39 @@ require("lazy").setup(
 		{"nvim-treesitter/nvim-treesitter-context",
 		event="VimEnter"},
 
-		{"EEprotocol/Arduineovim"}
+		{"EEprotocol/Arduineovim"},
+		{"skanehira/preview-markdown.vim"}
 	}
 )
 -------------------------------------------------------------------------------
 --Arduienovim
 -------------------------------------------------------------------------------
+require('arduineovim').setup()
+-------------------------------------------------------------------------------
+--Markdown previewer (peek)
+-------------------------------------------------------------------------------
+require('peek').setup({
+  auto_load = true,         -- whether to automatically load preview when
+                            -- entering another markdown buffer
+  close_on_bdelete = true,  -- close preview window on buffer delete
+
+  syntax = true,            -- enable syntax highlighting, affects performance
+
+  theme = 'dark',           -- 'dark' or 'light'
+
+  update_on_change = true,
+
+  app = 'webview',          -- 'webview', 'browser', string or a table of strings
+                            -- explained below
+
+  filetype = { 'markdown' },-- list of filetypes to recognize as markdown
+
+  -- relevant if update_on_change is true
+  throttle_at = 200000,     -- start throttling when file exceeds this
+                            -- amount of bytes in size
+  throttle_time = 'auto',   -- minimum amount of time in milliseconds
+                            -- that has to pass before starting new render
+})
 -------------------------------------------------------------------------------
 --LSP setting
 -------------------------------------------------------------------------------
