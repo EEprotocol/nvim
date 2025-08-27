4abcd63a0f4272287da17f35d82e22b08e71a805
EEprotocol <kei310c@outlook.com>
2025-08-27 15:07:32 +0900

    [Modify] changed the background color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -9,6 +9,8 @@ return{
 	sidebars={"qf","help"},
 	lualine_bold=true,
 	on_colors=function(colors)
+    colors.bg="#09136c"--background
+    colors.bg_highlight="#1e2671"
 		colors.fg_gutter="#ffba00"--number color
 		colors.fg_dark="#ff9955"--command line
 		colors.comment="#889fdd"--comment out
